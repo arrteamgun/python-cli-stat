3f9c8c6a49c1de0fab309f359c48178fd72f3ad7
Roman Timganov <romatimganov@gmail.com>
2023-07-27 19:56:31 +0400

    work with matrix quorters
    sum by quorters

--- python-cli-stat.py
+++ python-cli-stat.py
@@ -1,7 +1,7 @@
 import urllib.request
 url = "http://ftp.uk.debian.org/debian/dists/stable/main/"
 try:
-    urllib.request.urlretrieve(url, "tmp")
+    urllib.request.urlretrieve(url, "")
     print("success")
 except Exception as e:
     print(f"error {e}")
