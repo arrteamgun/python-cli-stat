e4a51a1d7765e1db31fed99b81ec0f314a9637ee
Roman Timganov <romatimganov@gmail.com>
2023-07-27 18:43:21 +0400

    matrix print

---
+++ python-cli-stat.py
@@ -0,0 +1,9 @@
+import urllib.request
+url = "http://ftp.uk.debian.org/debian/dists/stable/main/"
+try:
+    urllib.request.urlretrieve(url, "tmp")
+    print("success")
+except Exception as e:
+    print(f"error {e}")
+
+arch = input()
